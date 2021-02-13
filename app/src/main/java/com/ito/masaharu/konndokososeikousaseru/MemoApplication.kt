9162dda0fd8242b245451a6f89196acb86a25d8c
masaharu <ma3.ito.1225@gmail.com>
2021-02-13 15:02:13 +0900

    first commit

---
+++ app/src/main/java/com/ito/masaharu/konndokososeikousaseru/MemoApplication.kt
@@ -0,0 +1,17 @@
+package com.ito.masaharu.konndokososeikousaseru
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class MemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
+
+}
